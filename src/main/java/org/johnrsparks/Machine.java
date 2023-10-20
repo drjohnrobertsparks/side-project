03deab0e8c43aa88189d89df7386cdf37ffa49cf
John Sparks <johnrobertsparks@icloud.com>
2023-10-20 08:52:16 -0400

    test

---
+++ src/main/java/org/johnrsparks/Machine.java
@@ -0,0 +1,4 @@
+package org.johnrsparks;
+
+public class Machine {
+}
---
+++ src/main/java/org/johnrsparks/MachineCLI.java
@@ -0,0 +1,7 @@
+package org.johnrsparks;
+
+public class MachineCLI {
+    public static void main(String[] args) {
+
+    }
+}
